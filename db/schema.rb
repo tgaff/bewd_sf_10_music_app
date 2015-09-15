55f6e081b02771a346541e20d35f0921b40f88ad
tgaff <tgaff@alumni.nd.edu>
2015-09-14 21:14:04 -0700

    run 'rake db:migrate && annotate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915040052) do
+ActiveRecord::Schema.define(version: 20150915041130) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,16 @@ ActiveRecord::Schema.define(version: 20150915040052) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "length"
+    t.string   "genre"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
 
 end
