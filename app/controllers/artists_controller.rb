39587b63f03f3f220c0b752beb003e8c28c22bdc
tgaff <tgaff@alumni.nd.edu>
2015-09-16 21:25:20 -0700

    add the get_artist method and fill in update

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,10 +13,25 @@ class ArtistsController < ApplicationController
   def edit
   end
 
+
+  def update
+    @artist = get_artist
+    if @artist.update(artist_params)
+      redirect_to artist_path(@artist), notice: "Artist removed"
+    else
+      render "edit"
+    end
+  end
+
+
 private
 
-# for great safety!
+  # for great safety!
   def artist_params
     params.require(:artist).permit(:name, :image_url, :description)
+  end
+
+  def get_artist
+    Artist.find(params[:id])
   end
 end
